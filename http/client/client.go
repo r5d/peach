6c777e540a9c15eea62407aa878be855a2e8e6e4
siddharth <s@ricketyspace.net>
2022-04-24 00:05:25 -0400

    http/client: flesh out Get

---
+++ http/client/client.go
@@ -0,0 +1,31 @@
+// Copyright © 2022 siddharth <s@ricketyspace.net>
+// SPDX-License-Identifier: ISC
+
+// Thin HTTP client wrapper.
+package client
+
+import (
+	"net/http"
+
+	"ricketyspace.net/peach/version"
+)
+
+// HTTP client.
+var client = http.Client{}
+
+// Make a HTTP GET request.
+func Get(url string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = buildHeaders(req)
+
+	return client.Do(req)
+}
+
+// Add default headers for the peach http client.
+func buildHeaders(req *http.Request) *http.Request {
+	req.Header.Set("User-Agent", "peach/"+version.Version+" ricketyspace.net/contact")
+	return req
+}
